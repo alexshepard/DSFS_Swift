afb1f5074859fbd6c60d052d2f70c41ca2e3064e
Alex Shepard <alex@meatfreezer.com>
2024-05-23 13:43:25 -0700

    inverse normal cdf
    also, rename args to mean and standardDeviation
    and switch to an extension on Real : BinaryFloatingPoint
    since doing these statistics on integers doesn't make much
    sense

--- Ch6_Probability_Numerics.playground/Pages/Normal Distribution Numerics.xcplaygroundpage/Contents.swift
+++ Ch6_Probability_Numerics.playground/Pages/Normal Distribution Numerics.xcplaygroundpage/Contents.swift
@@ -7,19 +7,46 @@ import SwiftUI
 import Charts
 import PlaygroundSupport
 
-extension Real {
-    func normalPdf(mu: Self = 0, sigma: Self = 1) -> Self {
+extension Real where Self: BinaryFloatingPoint {
+    func normalPdf(mean mu: Self = 0, standardDeviation sigma: Self = 1) -> Self {
+        let exponent = Self.exp(-((self - mu) * (self - mu)) / (2 * sigma * sigma))
         return Self.exp(-((self - mu) * (self - mu)) / (2 * sigma * sigma)) / ((Self.pi * 2).squareRoot() * sigma)
     }
     
-    func normalCdf(mu: Self = 0, sigma: Self = 1) -> Self {
+    func normalCdf(mean mu: Self = 0, standardDeviation sigma: Self = 1) -> Self {
         return (1 + Self.erf((self - mu) / Self(2).squareRoot() / sigma)) / 2
+    }
+
+    func inverseNormalCdf(mean mu: Self = 0, standardDeviation sigma: Self = 1, tolerance: Self = 0.00001) -> Self {
+        if mu != 0 || sigma != 1 {
+            return mu + sigma * self.inverseNormalCdf(mean: 0, standardDeviation: 1, tolerance: tolerance)
+        }
+        
+        // normal CDF of -10 is nearly zero, of 10 is nearly 1
+        var lowZ: Self = -10
+        var hiZ: Self = 10
+        var midZ: Self = 0
+        
+        while (hiZ - lowZ) > tolerance {
+            midZ = (lowZ + hiZ) / 2
+            let midP = midZ.normalCdf(mean: 0, standardDeviation: 1)
+            if midP < self {
+                lowZ = midZ
+            } else {
+                hiZ = midZ
+            }
+        }
+        
+        return midZ
     }
 }
 
-let x: Float = 0.3
+let x: Double = 0.5
 print(x.normalPdf())
 print(x.normalCdf())
+print(x.inverseNormalCdf(tolerance: 0.00001))
+print(x.normalCdf().inverseNormalCdf(tolerance: 0.00001))
+
 
 /// # let's chart some variants of the normal distribution
 struct Point: Identifiable {
@@ -32,10 +59,10 @@ struct Point: Identifiable {
 }
 
 let categories = [
-    (mu: 0.0, sigma: 1.0, label: "Mu: 0, Sigma: 1"),
-    (mu: 0.0, sigma: 2.0, label: "Mu: 0, Sigma: 2"),
-    (mu: 0.0, sigma: 0.5, label: "Mu: 0, Sigma: 0.5"),
-    (mu: -1.0, sigma: 1.0, label: "Mu: -1, Sigma: 1")
+    (mean: 0.0,  standardDeviation: 1.0, label: "Mean: 0, Standard Deviation: 1"),
+    (mean: 0.0,  standardDeviation: 2.0, label: "Mean: 0, Standard Deviation: 2"),
+    (mean: 0.0,  standardDeviation: 0.5, label: "Mean: 0, Standard Deviation: 0.5"),
+    (mean: -1.0, standardDeviation: 1.0, label: "Mean: -1, Standard Deviation: 1")
 ]
 
 let xs = stride(from: -5, to: 5, by: 0.1)
@@ -43,12 +70,13 @@ let points = xs.flatMap { x -> [Point] in
     return categories.map { category in
         Point(
             x: Double(x),
-            pdf: Double(x).normalPdf(mu: category.mu, sigma: category.sigma),
-            cdf: Double(x).normalCdf(mu: category.mu, sigma: category.sigma),
+            pdf: Double(x).normalPdf(mean: category.mean, standardDeviation: category.standardDeviation),
+            cdf: Double(x).normalCdf(mean: category.mean, standardDeviation: category.standardDeviation),
             category: category.label
         )
     }
 }
+
 
 struct ChartView: View {
     @State private var fn = "cdf"
@@ -63,6 +91,7 @@ struct ChartView: View {
                 }
             }
             .pickerStyle(.inline)
+            
             Chart(points) {
                 LineMark(
                     x: .value("x", $0.x),
